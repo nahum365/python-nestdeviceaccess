e5a5dde0df7137ea7cae842fc8a994038fd2b4a9
nahum365 <nahum365@me.com>
2020-12-09 18:52:36 -0500

    Change max line length to 120

--- nestdeviceaccess/test_nestdeviceaccess.py
+++ nestdeviceaccess/test_nestdeviceaccess.py
@@ -9,4 +9,3 @@ def callback():
 def test_require_all_arguments():
     with pytest.raises(nestdeviceaccess.ArgumentsMissingError):
         _ = nestdeviceaccess.NestDeviceAccess(project_id="", client_secret="", client_id="", code="")
-
