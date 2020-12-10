4fdb72ad76ec942b8eac5a3cee8bcc377796e1a0
nahum365 <nahum365@me.com>
2020-12-09 19:25:48 -0500

    Add tests for coverage

--- nestdeviceaccess/test_nestdeviceaccess.py
+++ nestdeviceaccess/test_nestdeviceaccess.py
@@ -12,16 +12,25 @@ def test_require_all_arguments():
 
 
 def test_url_printed_when_auth_code_not_present(capsys):
-    with pytest.raises(nestdeviceaccess.ArgumentsMissingError):
+    try:
         _ = nestdeviceaccess.NestDeviceAccess(project_id="123", client_secret="123", client_id="123", code="")
-        captured = capsys.readouterr()
-        assert captured.out == "Go to this link to get OAuth token: " \
-                               "https://nestservices.google.com/partnerconnections/123/auth?redirect_uri=https://www" \
-                               ".google.com&access_type=offline&prompt=consent&client_id=123&response_type=code&scope" \
-                               "=https://www.googleapis.com/auth/sdm.service\n"
+    except nestdeviceaccess.ArgumentsMissingError:
+        pass
+    captured = capsys.readouterr()
+    assert captured.out == "Go to this link to get OAuth token: " \
+                           "https://nestservices.google.com/partnerconnections/123/auth?redirect_uri=https://www" \
+                           ".google.com&access_type=offline&prompt=consent&client_id=123&response_type=code&scope" \
+                           "=https://www.googleapis.com/auth/sdm.service\n"
 
 
 def test_devices_call_without_login_fails():
     with pytest.raises(nestdeviceaccess.AuthorizationError):
         nda = nestdeviceaccess.NestDeviceAccess(project_id="123", client_secret="123", client_id="123", code="123")
         nda.devices()
+
+
+def test_failed_login_prints_error(capsys):
+    nda = nestdeviceaccess.NestDeviceAccess(project_id="123", client_secret="123", client_id="123", code="123")
+    nda.login()
+    captured = capsys.readouterr()
+    assert captured.out == "Authorization Error\n"
